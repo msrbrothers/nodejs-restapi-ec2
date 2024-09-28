c1f0ac818d5a343142fb8561239bfe2d78943e09
Ravindra <43512539+msrbrothers@users.noreply.github.com>
2024-09-28 22:26:06 +0530

    Update index.js "get user api"

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // GET all users
-app.get('/users', (req, res) => {
+app.get('/api/users', (req, res) => {
   res.json(users);
 });
 
